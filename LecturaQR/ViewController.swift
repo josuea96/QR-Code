7e0aa110bae8e0ac31c6d83e8fa5598ae94a4a77
ITESM CAMPUS TAMPICO <Administrador@graphic-design-17.local>
2016-09-28 08:55:24 -0500

    Initial Commit

---
+++ LecturaQR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LecturaQR
+//
+//  Created by ITESM CAMPUS TAMPICO on 28/09/16.
+//  Copyright © 2016 ITESM CAMPUS TAMPICO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
